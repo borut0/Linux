8d8f9771a5e403393dc6b670f0dd32a1c9efb826
borut0 <89539128+borut0@users.noreply.github.com>
2023-02-14 15:22:03 +0530

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,25 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+end
+
+# aliases
+
+alias ..='cd ..'
+alias vim='nvim'
+alias vi='nvim'
+
+# fish startup
+
+# Start X at login
+if status --is-login
+  if test -z "$DISPLAY" -a $XDG_VTNR = 1
+    exec startx -- -keeptty
+  end
+end
+
+
+# starship
+starship init fish | source
